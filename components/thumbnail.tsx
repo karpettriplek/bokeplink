02230357808675ed2f26741fb7062dcb13b6455a
karpettriplek <161988986+karpettriplek@users.noreply.github.com>
2024-04-09 20:47:39 +0700

    Update thumbnail.tsx

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import { useState } from "react";
 
 const ImageLoader = ({ src, width, quality }: any) => {
-    return `${src}`;
+    return `${src}&w=320&q=${
+        quality || 80
+    }`;
 };
-
 const Thumbnail = ({
     single_img,
     splash_img,
